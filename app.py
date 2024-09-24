d4b135e96d2e0f2451d974f538bf1b357fb7b742
humzava <humzafidava@gmail.com>
2024-09-25 01:33:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -476,7 +476,7 @@ match __name__:
         print(terminalASCII())
 
         # Run the app with the debug mode, host and port settings
-        app.run(host=APP_HOST, port=APP_PORT)
+        app.run(host=0.0.0.0;, port=80)
 
         # Get the end time of the app
         endTime = currentTimeStamp()
